abd48e349366a61e77698594ba5e2f7dc9e37b47
Cary Guca <cary.guca@gmail.com>
2020-04-05 16:10:42 -0500

    Added functionality

---
+++ module2-solution/app.js
@@ -0,0 +1,58 @@
+(function () {
+    'use strict';
+    
+    angular.module('ShoppingListCheckOff', [])
+    .controller('ToBuyController', ToBuyController)
+    .controller('AlreadyBoughtController', AlreadyBoughtController)
+    .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+    
+    ToBuyController.$inject = ['ShoppingListCheckOffService'];
+    function ToBuyController(ShoppingListCheckOffService) {
+      var itemAdder = this;
+
+      itemAdder.items = ShoppingListCheckOffService.getItems();
+
+      itemAdder.buyItem = function (itemIndex) {
+        ShoppingListCheckOffService.buyItem(itemIndex);
+      };
+    }
+    
+    
+    AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+    function AlreadyBoughtController(ShoppingListCheckOffService) {
+      var showList = this;
+    
+      showList.boughtItems = ShoppingListCheckOffService.getBoughtItems();
+    }
+    
+    
+    function ShoppingListCheckOffService() {
+      var service = this;
+    
+      // List of shopping items
+      var toBuyList = [
+        {name: "Milk",quantity: "2"},
+        {name: "Donuts",quantity: "200"},
+        {name: "Cookies",quantity: "300"},
+        {name: "Chocolate", quantity: "5"}
+      ];
+
+      var boughtList = [];
+
+      service.buyItem = function (itemIndex) {
+        var boughtItem = toBuyList[itemIndex];
+        boughtList.push(boughtItem);
+        toBuyList.splice(itemIndex, 1);
+      };
+    
+      service.getItems = function () {
+        return toBuyList;
+      };
+
+      service.getBoughtItems = function () {
+        return boughtList;
+      };
+    }
+    
+    })();
+    
